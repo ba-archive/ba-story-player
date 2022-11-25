3106cca9e0f0f73098b262d490cd293f371e5d44
NENCAO <56115666@qq.com>
2022-11-25 17:23:30 +0800

    feat: 添加对话框组件

--- lib/BaStoryPlayer.vue
+++ lib/BaStoryPlayer.vue
@@ -2,6 +2,9 @@
 import { onMounted } from 'vue';
 import { init } from '@/index';
 import { StoryRawUnit  } from '@/types/common';
+import eventBus from "@/eventBus";
+import BaDialog from "@/layers/textLayer/BaDialog.vue";
+
 let { story, dataUrl, height, width } = defineProps<{
   story: StoryRawUnit[]
   dataUrl: string
@@ -10,16 +13,39 @@ let { story, dataUrl, height, width } = defineProps<{
 }>()
 
 onMounted(() => { init('player__main', height, width, story, dataUrl) })
+
+/**
+ * 测试文本框
+ * @constructor
+ */
+function TestDialog(){
+  eventBus.emit('showText', {
+    text: [{content: "测试文本asjdklajsdlkjlaskd", waitTime: 1000}],
+    speaker: {
+      name: "未花",
+      nickName: "茶话会"
+    },
+    textEffect: []
+  });
+}
+
 </script>
 
 <template>
   <div id="player" :style="{height:`${height}px`}">
-    <div id="player__main"></div>
+    <div id="player__main">
+      <BaDialog class="dialog" :dialog-height="height" :style="{marginTop: `${height / 2}px`, width:`${width}px`, height:`${height / 2}px`}"></BaDialog>
+    </div>
+    <button @click="TestDialog">文本框测试</button>
   </div>
 </template>
 
 <style>
 #player{
   background-color: red;
+}
+
+.dialog{
+  position: absolute;
 }
 </style>
---
+++ lib/layers/textLayer/BaDialog.vue
@@ -0,0 +1,102 @@
+<template>
+  <div :style="{padding: `${3 * props.dialogHeight / 1080}rem ${8 * props.dialogHeight / 1080}rem`}" class="dialog">
+    <div class="title">
+      <div :style="{fontSize: `${3.5 * props.dialogHeight / 1080}rem`}"  class="name">{{name}}</div>
+      <div :style="{fontSize: `${2 * props.dialogHeight / 1080}rem`}" class="department">{{nickName}}</div>
+    </div>
+    <hr>
+    <div :style="{fontSize: `${2.5 * props.dialogHeight / 1080}rem`}" class="content">{{ obj.output }}</div>
+
+  </div>
+
+</template>
+
+<script setup>
+
+import {reactive, onMounted, ref} from 'vue'
+import EasyTyper from 'easy-typer-js'
+
+
+// 计算属性
+const obj = reactive({
+  // 忽略
+  output: '',
+  // 忽略
+  isEnd: false,
+  // 打字间隔
+  speed: 20,
+  // 关闭否则会消除之前的字体
+  singleBack: false,
+  // 忽略
+  sleep: 0,
+  // 固定
+  type: 'normal',
+  // 忽略
+  backSpeed: 40,
+  // 忽略
+  sentencePause: false
+})
+
+// 外部传入播放器高度,用于动态计算字体等数值
+const props = defineProps({
+  dialogHeight: Number
+})
+
+// 昵称
+let name = ref('').value;
+// 次级标题(昵称右边)
+let nickName = ref('').value;
+
+onMounted(() => {
+
+})
+
+// 监听text事件
+eventBus.on('showText',
+  (e) => {
+    // 设置内容
+    new EasyTyper(obj, e.text[0].content);
+    // 设置昵称
+    name = e.speaker.name;
+    // 设置次级标题
+    nickName = e.speaker.nickName;
+});
+
+</script>
+
+<style scoped>
+.name{
+  font-family: 'TJL',serif;
+  font-size: 3.5rem;
+  color : white;
+  align-self: flex-end;
+}
+
+.department{
+  font-family: 'TJL',serif;
+  margin-left: 2rem;
+  font-size: 2.5rem;
+  color : rgb(156,218,240);
+}
+
+.title{
+  display: flex;
+  align-items: flex-end;
+  margin-top: 2rem;
+}
+
+.dialog{
+  width: 100vw;
+  height: 30vh;
+  padding: 3rem 8rem;
+  box-sizing: border-box;
+  background-image: linear-gradient(to bottom , rgba(255,0,0,0) , rgba(19,32,45,0.9) 30%);
+}
+
+.content{
+  margin-top: 1.5rem;
+  font-family: 'TJL',serif;
+  color : white;
+  font-size: 2.5rem;
+}
+</style>
