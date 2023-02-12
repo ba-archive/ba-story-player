97cffad01246c749821175ec21e2817fc558b522
ourandream <ouranhuang@gmail.com>
2023-02-12 10:37:12 +0800

    fix: 修复了某些情况player__main会被拉长的问题

--- lib/BaStoryPlayer.vue
+++ lib/BaStoryPlayer.vue
@@ -29,7 +29,7 @@ onMounted(() => { init('player__main__canvas', playerConfig, () => emitter('end'
 </script>
 
 <template>
-  <div id="player">
+  <div id="player" :style="{ height: `${height}px`, width: `${width}px` }">
     <div id="player__main" :style="{ height: `${height}px`, width: `${width}px` }">
       <div id="player__main__canvas" :style="{ transform: `scale(${scale})` }"></div>
       <BaDialog :player-height="height" :player-width="width" :style="{ width: `${width}px` }"></BaDialog>
