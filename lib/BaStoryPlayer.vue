1bc81b6812ce27f5755da356d96dd73caf9e5cf0
ourandream <ouranhuang@gmail.com>
2023-02-12 09:45:18 +0800

    chore: 移除了播放器组件定义里的测试代码

--- lib/BaStoryPlayer.vue
+++ lib/BaStoryPlayer.vue
@@ -3,7 +3,6 @@ import eventBus from "@/eventBus";
 import { init } from '@/index';
 import { StoryRawUnit } from '@/types/common';
 import { onMounted } from 'vue';
-//@ts-ignore
 import BaDialog from "@/layers/textLayer/BaDialog.vue";
 import { Language, StorySummary } from '@/types/store';
 
@@ -36,62 +35,6 @@ function testDialog() {
   });
 }
 
-function testSt() {
-  eventBus.emit("st", {
-    text: [{
-      content: "― 哈、",
-      waitTime: 0,
-      effects: []
-    },{
-      content: "哈……",
-      waitTime: 1500,
-      effects: []
-    }],
-    stArgs: [[-1200,-530], "serial", 1]
-  })
-}
-function testSt2() {
-  eventBus.emit("st", {
-    text: [{
-      content: "",
-      waitTime: 0,
-      effects: []
-    },{
-      content: "― 没想到",
-      waitTime: 1800,
-      effects: []
-    },{
-      content: "只过了",
-      waitTime: 500,
-      effects: []
-    },{
-      content: "几个月",
-      waitTime: 300,
-      effects: []
-    },{
-      content: "时间",
-      waitTime: 600,
-      effects: []
-    },{
-      content: "体力",
-      waitTime: 1100,
-      effects: []
-    },{
-      content: "就下降",
-      waitTime: 500,
-      effects: []
-    },{
-      content: "到了",
-      waitTime: 500,
-      effects: []
-    },{
-      content: "这种程度……",
-      waitTime: 200,
-      effects: []
-    }],
-    stArgs: [[-1200,-630], "serial", 1]
-  })
-}
 
 function clearSt() {
   eventBus.emit("clearSt");
@@ -102,14 +45,8 @@ function clearSt() {
 <template>
   <div id="player" :style="{ height: `${height}px` }">
     <div id="player__main">
-      <BaDialog :player-height="height" :player-width="width"
-        :style="{ width: `${width}px` }"></BaDialog>
+      <BaDialog :player-height="height" :player-width="width" :style="{ width: `${width}px` }"></BaDialog>
     </div>
-    <button @click="testDialog">文本框测试</button>
-    <button style="margin-left:1%" @click="testSt">st测试</button>
-    <button style="margin-left:1%" @click="testSt2">st测试2</button>
-    <button style="margin-left:1%" @click="clearSt">clearSt</button>
-    <button style="margin-left:1%" @click="() => {  eventBus.emit('next') }">next</button>
   </div>
 </template>
 
