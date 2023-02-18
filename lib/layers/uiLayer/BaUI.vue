3c7a2a0850129e1d18af00450377eb5efbb029eb
notnotype <notnotype@gmail.com>
2023-02-18 13:41:26 +0800

    fix: change uiLayer z-index to 100

--- lib/layers/uiLayer/BaUI.vue
+++ lib/layers/uiLayer/BaUI.vue
@@ -148,7 +148,6 @@ onMounted(() => {
   position: absolute;
   top: 0;
   right: 0;
-  z-index: 100;
   padding: 1.5%;
   user-select: none;
 }
@@ -158,6 +157,7 @@ onMounted(() => {
   width: 100%;
   height: 100%;
   top: 0;
+  z-index: 100;
 
   .baui-button-group {
     display: grid;
